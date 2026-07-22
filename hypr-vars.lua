87843e8016ab0a8b8fc31c888eedff26e5d05cda
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-07-22 16:39:07 -0500

    fix(vars): switch cm_auto_hdr back to 1

--- hypr-vars.lua
+++ hypr-vars.lua
@@ -143,7 +143,7 @@ return {
     },
     render = {
         cm_sdr_eotf = "gamma22force",
-        cm_auto_hdr = 2,
+        cm_auto_hdr = 1,
         use_shader_blur_blend = true,
     },
     rules = {
